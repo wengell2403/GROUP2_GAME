34742ac1b80451c3c98057457e1195f38fd5d918
wengell2403 <wengrqn@gmail.com>
2025-03-25 11:31:43 +0800

    march 25 2023
    added lvl 1 bsn, added char select button bsn

--- Scene/Menu/character_select.gd
+++ Scene/Menu/character_select.gd
@@ -9,5 +9,13 @@ func _on_level_1_bshm_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scene/Levels/level1_bshm.tscn")
 
 
-func _on_button_pressed() -> void:
+func _on_level_1_bstm_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scene/Levels/level1_bstm.tscn")
+
+
+func _on_level_1_bsn_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/Levels/level1_bsn.tscn")
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
